9145a5e9a708d152dc76a6f17798a081557f10d5
jiyun kwon <20087623@mail.wit.ie>
2024-10-08 18:01:25 +0100

    Mark task as done

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -6,23 +6,35 @@ import React,{ useState } from 'react';
 function App() {
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { id: 3, title: "Tidy up", deadline: "Today" }
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
+      { id: 3, title: "Tidy up", deadline: "Today", done: false}
     ]
   });
+
+  const doneHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    setTaskState({tasks});
+    console.log(`${taskIndex} ${tasks[taskIndex].done}`);}
+
+
   return (
     <div className="container">
       <h1>Tasky</h1>
-      {taskState.tasks.map((task) => (
+      {taskState.tasks.map((task, index) => (
       <Task 
       title={task.title} 
       deadline={task.deadline}
-       description={task.description}
-       key ={task.id} />
+      description={task.description}
+      
+      done={task.done}
+      key ={task.id}
+      markDone={() => doneHandler(index)} 
+      />
       ))}
    </div>
   );
-}
 
+}
 export default App;
--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -3,10 +3,12 @@ import React from 'react';
 const Task = (props) => {
     
     return (
-        <div className='card'>
+        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
+
         <p className="title">{props.title}</p>
         <p>Due: {props.deadline}</p>
         <p>{props.description}</p>
+        <button onClick={props.markDone}>Done</button>
         </div>
     )
 }
